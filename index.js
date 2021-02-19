5cb9b02da5e263fff2170196099546f89b36e582
okorkov <alex.okorkov@gmail.com>
2021-02-18 16:04:22 -0800

    commit tree 13344df0046be087bacc0db6d6aabbd4be1d6503

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+
+function map(array, callBack) {
+  let result = [];
+  for(let i = 0; i < array.length; i++) {
+    result.push(callBack(array[i]))
+  }
+  return result;
+}
+
+function reduce(array, callBack, startingPoint=0) {
+  for(let i = 0; i < array.length; i++) {
+    let placeholder;
+    if(typeof array[i]  === 'number') {
+      placeholder = startingPoint;
+    } else {
+      placeholder = -1;
+    }
+    startingPoint = callBack(array[i],placeholder)
+  }
+  return startingPoint;
+}
